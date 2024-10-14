fixed the ++ or +- in ctot calculation for three body reactions

chemgen/chemistry.py
@@ -261,7 +261,7 @@ class chemistry_expressions:
                     + (f" * (T ** ({beta}))" if abs(beta) > 0.0 else "") \
                     + f" * np.exp({-Ea:.15e} / (Rc * T))"
             expr["kb"] = f"kf * ({eqk})" if reaction['reversible'] else "0"
-            expr["rr"] = f"(ctot " + ("+" if len(reaction["third_body"]) > 0 else "") \
+            expr["rr"] = f"(ctot " \
                         + " ".join([("+" if eff > 0.0 else "-")+f"{abs(eff):.15e}*C[{self.chem.reduced_species_index(sp)}]\\\n    " for sp,eff in reaction["third_body"].items()])\
                          + f")*(kf * ({reactants_expr}) - kb * ({products_expr}))"
         elif reaction["type"] == "troe":

tests/write_getrates/test_getrates.py
@@ -56,9 +56,9 @@ P_array = np.zeros((veclen,))
 P_array[:] = P_cgs
 Y_array = np.zeros_like(wdot_ftn)
 Y_array[0,:] = Y[:]
-wdot_ftn = getrates_ftn(T_array, Y_array, P_array,veclen)
+# wdot_ftn = getrates_ftn(T_array, Y_array, P_array,veclen)
 print(wdot_ftn.shape)
-# getrates_python(T, Y, P_cgs , wdot_python)
+getrates_python(T, Y, P_cgs , wdot_python)
 
 # print("diff bn dims",np.amax(wdot[0]-wdot[-1]),np.amin(wdot[0]-wdot[-1]))
 custom_wdot.append(wdot_ftn[0])
@@ -122,5 +122,5 @@ def compare_wdot(cantera_wdot, custom_wdot, tolerance=1e-6):
 # Run comparison
 print("ftn error")
 compare_wdot(cantera_wdot, custom_wdot[0])
-# print("python error")
-# compare_wdot(cantera_wdot, custom_wdot[1])
+print("python error")
+compare_wdot(cantera_wdot, custom_wdot[1])